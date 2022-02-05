b36ff87526469d0dc38a4eee5daf7cca433318e8
1zabreado <sergiufulop45@gmail.com>
2022-02-05 12:05:59 +0200

    objects

--- TS_files/app.ts
+++ TS_files/app.ts
@@ -9,32 +9,33 @@
 //     hobbies: ["gaming", "anime", "coding"],
 //     role: [2, 'author']
 // };
+
 // const ADMIN = 0;
 // const READ_ONLY = 1;
 // const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var user = {
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const user = {
     name: "za breado#0001",
     age: 16,
     hobbies: ["gaming", "anime", "coding"],
     role: Role.ADMIN
 };
+
 // user.role.push('admin'); - this works but not recommended
 // user.role[1] = 10;
-var favouriteActivities;
+
+let favouriteActivities: string[];
 favouriteActivities = user.hobbies;
+
 console.log(user.name);
-for (var _i = 0, _a = user.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+
+for (const hobby of user.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map())
 }
+
 if (user.role === Role.ADMIN) {
-    console.log("".concat(user.name, " is an admin"));
+    console.log(`${user.name} is an admin`);
 }
