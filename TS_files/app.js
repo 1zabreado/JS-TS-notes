7b3c9373b6482f86714a890c2f7401a3b46f839a
1zabreado <sergiufulop45@gmail.com>
2022-02-06 13:03:02 +0200

    never

--- TS_files/app.js
+++ TS_files/app.js
@@ -1,20 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Ana";
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 10));
-var combinedValues;
-combinedValues = add;
-// combinedValues = printResult;
-// combinedValues = 5;
-console.log(combinedValues(8, 8));
-// let someValue: undefined;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError('An error occured', 300);
+console.log(result);
